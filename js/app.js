6f8f8035fc4fab0053ff0dff822c71c557631fbe
ejiogurobert <66321328+ejiogurobert@users.noreply.github.com>
2021-08-19 02:36:15 +0100

    Done with profile page

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const showSideBar = () => {
+  let element = document.getElementById('sideBar');
+  element.style.left = '30%';
+};
+const hideSideBar = () => {
+  let element = document.getElementById('sideBar');
+  element.style.left = '100%';
+}
